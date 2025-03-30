8779f04008259655ba979a5b548014e43e8a7cad
baabar02 <baabarmx@gmail.com>
2025-03-30 18:50:54 +0800

    jjj

--- node/file.js
+++ node/file.js
@@ -1,82 +1,175 @@
-// const fs = require("fs");
+// const fs = require('fs');
 
-// const lebron = {
-//   about: "LeBron Raymone James Sr. ",
-//   age: 39,
-//   height: 206,
+// const dataMain = {
+//     name: 'bbb',
+//     age: '25'
+// }
+
+// const dataBrings = async () => {
+//     const dataTojs = await fs.readFileSync('./database.json', 'utf-8');
+//     const dataToob = JSON.parse(dataTojs);  
+//     return dataToob;
+// } 
+
+// const dataAdds = async (dataMain) =>{
+//     const dataCall = await dataBrings();
+//     dataCall.push(dataMain);
+//     const dataJs = JSON.stringify(dataCall);
+//     await fs.writeFileSync('./database.json', dataJs, 'utf-8');
+
 // };
 
-// const bidniiDatabaseDeerBaigaaYmiigAvchrag = async () => {
-//   const bidniiDataJSON = await fs.readFileSync(
-//     "./bidniiDatabase.json",
-//     "utf-8"
-//   );
-//   const bidniiDataObjectBolonHuvirsan = JSON.parse(bidniiDataJSON);
-//   return bidniiDataObjectBolonHuvirsan;
+// dataAdds(dataMain);
+
+
+
+// const fs = require('fs').promises;
+
+// const dataInput = {
+//   name: 'jjj',
+//   age: 40
 // };
 
-// const lebron1 = {
-//   about: "LeBron Raymone James Sr. ",
-//   age: 39,
-//   height: 206,
+// const dataBrings = async () => {
+//   try {
+//     const dataJ = await fs.readFile('./database.json', 'utf-8');
+//     const dataOb = JSON.parse(dataJ);
+    
+//     if (!Array.isArray(dataOb)) {
+//       throw new Error('Database must contain an array');
+//     }
+    
+//     console.log('[READ] Successfully loaded:', JSON.stringify(dataOb, null, 2));
+//     return dataOb;
+//   } catch (error) {
+//     if (error.code === 'ENOENT') {
+//       console.log('[READ] No database found, initializing empty array');
+//       return [];
+//     }
+//     throw new Error(`[READ] Error: ${error.message}`);
+//   }
 // };
 
-// // const playerOneLogin = { username: "Player1", password: "asd123" };
-
-// const bidniiDatabaseDeerYmNemeh = async () => {
-//   const bidendBaigaaPlayers = await bidniiDatabaseDeerBaigaaYmiigAvchrag();
-
-//   //   const findPlayerOne = bidendBaigaaPlayers.find(
-//   //     (el) => el.username === playerOneLogin.username
-//   //   );
-
-//   //   if (findPlayerOne != undefined) {
-//   //     if (findPlayerOne.password === playerOneLogin.password) {
-//   bidendBaigaaPlayers.push(lebron);
-//   const JSONBidendBaigaaPlayers = JSON.stringify(bidendBaigaaPlayers);
-
-//   await fs.writeFileSync(
-//     "./bidniiDatabase.json",
-//     JSONBidendBaigaaPlayers,
-//     "utf-8"
-//   );
-//   //     } else {
-//   //       console.log("Wrong password passwordoo sana");
-//   //     }
-//   //   } else {
-//   //     console.log("Erh alga not authorized");
-//   //   }
+// const dataAdd = async (dataInput) => {
+//   try {
+//     if (!dataInput || typeof dataInput !== 'object') {
+//       throw new Error('Invalid input data');
+//     }
+    
+//     const dataCall = await dataBrings();
+//     console.log('[ADD] Before:', JSON.stringify(dataCall, null, 2));
+    
+//     dataCall.push(dataInput);
+//     console.log('[ADD] After:', JSON.stringify(dataCall, null, 2));
+    
+//     const dataJS = JSON.stringify(dataCall, null, 2);
+//     await fs.writeFile('./database.json', dataJS, 'utf-8');
+    
+//     console.log('[ADD] Success: Data written to file');
+//     return true;
+//   } catch (error) {
+//     throw new Error(`[ADD] Error: ${error.message}`);
+//   }
 // };
 
-// bidniiDatabaseDeerYmNemeh();
+// // New data removal function
+// const dataRemove = async (nameToRemove) => {
+//   try {
+//     const dataCall = await dataBrings();
+//     console.log('[REMOVE] Before:', JSON.stringify(dataCall, null, 2));
+    
+//     const initialLength = dataCall.length;
+//     const updatedData = dataCall.filter(item => item.name !== nameToRemove);
+    
+//     if (initialLength === updatedData.length) {
+//       console.log('[REMOVE] No matching entry found for:', nameToRemove);
+//       return false;
+//     }
+    
+//     console.log('[REMOVE] After:', JSON.stringify(updatedData, null, 2));
+//     await fs.writeFile('./database.json', JSON.stringify(updatedData, null, 2), 'utf-8');
+    
+//     console.log('[REMOVE] Success: Removed entry with name:', nameToRemove);
+//     return true;
+//   } catch (error) {
+//     throw new Error(`[REMOVE] Error: ${error.message}`);
+//   }
+// };
+
+// const verifyDatabase = async () => {
+//   try {
+//     const contents = await fs.readFile('./database.json', 'utf-8');
+//     console.log('[VERIFY] Current contents:', contents);
+//   } catch (error) {
+//     console.error('[VERIFY] Error:', error.message);
+//   }
+// };
+
+// // Test scenario with error case and removal
+// (async () => {
+//   try {
+//     console.log('[TEST] Starting with input:', JSON.stringify(dataInput, null, 2));
+    
+//     // Add data
+//     await dataAdd(dataInput);
+    
+//     // Test error case: invalid input
+//     try {
+//       await dataAdd(null);
+//     } catch (error) {
+//       console.log('[TEST] Expected error caught:', error.message);
+//     }
+    
+//     // Remove data
+//     await dataRemove('jjj');
+    
+//     // Verify final state
+//     await verifyDatabase();
+    
+//     console.log('[TEST] All operations completed');
+//   } catch (error) {
+//     console.error('[TEST] Failed:', error.message);
+//   }
+// })();
 
 const fs = require("fs");
 
-const lebron = {
-  about: "Baabar. ",
-  age: 25,
-  height: 226,
+const bidniiDatabaseDeerBaigaaYmiigAvchrag = async () => {
+  const bidniiDataJSON = await fs.readFileSync(
+    "./database.json",
+    "utf-8"
+  );
+  const bidniiDataObjectBolonHuvirsan = JSON.parse(bidniiDataJSON);
+  return bidniiDataObjectBolonHuvirsan;
 };
 
-const dataAvchirdgFunction = async () => {
-  const json1 = await fs.readFileSync("./bidniiDatabase.json", "utf-8");
-  const datoOb = JSON.parse(json1);
-  return datoOb; //function ii return ddudsan gazriin urd huvisagch irdeg
+const nextPlayer = { username: "player4", password: "hello" };
+const playerOneLogin = { username: "Player1", password: "asd123" };
+
+const bidniiDatabaseDeerYmNemeh = async () => {
+  const bidendBaigaaPlayers = await bidniiDatabaseDeerBaigaaYmiigAvchrag();
+
+  const findPlayerOne = bidendBaigaaPlayers.find(
+    (el) => el.username === playerOneLogin.username
+  );
+
+  if (findPlayerOne != undefined) {
+    if (findPlayerOne.password === playerOneLogin.password) {
+      bidendBaigaaPlayers.push(nextPlayer);
+      const JSONBidendBaigaaPlayers = JSON.stringify(bidendBaigaaPlayers);
+
+      await fs.writeFileSync(
+        "./database.json",
+        JSONBidendBaigaaPlayers,
+        "utf-8"
+      );
+    } else {
+      console.log("Wrong password passwordoo sana");
+    }
+  } else {
+    console.log("Erh alga not authorized");
+  }
 };
 
-const dataNemdegFunction = async (toglogch) => {
-  const data = await dataAvchirdgFunction();
-  data.push(toglogch);
-  const JSONbolsondata = JSON.stringify(data);
-  await fs.writeFileSync("./bidniiDatabase.json", JSONbolsondata, "utf-8");
-};
-
-dataNemdegFunction(lebron);
-
-// const nameee = () => {
-//   return "hello";
-// };
-
-// const data = nameee()
-//     //  ^
-//     // hello
+bidniiDatabaseDeerYmNemeh();
+ 
--- node/function.js
+++ node/function.js
@@ -1,40 +1,40 @@
-// const sayHi = () => {
-//   console.log("hi");
-// };
+const sayHi = () => {
+  console.log("hi");
+};
 
-// const sum = (a, b, c) => {
-//   return a + b + c;
-// };
-// const result = sum(10, 2, 3, 7);
-// console.log(result);
+const sum = (a, b, c) => {
+  return a + b + c;
+};
+const result = sum(10, 2, 3, 7);
+console.log(result);
 
-// // 1. Parameter -> huvisan orj irdeg ugugdul
-// // 2. Return -> Hiigdesen uildeliin hariu butsah
+// 1. Parameter -> huvisan orj irdeg ugugdul
+// 2. Return -> Hiigdesen uildeliin hariu butsah
 
-// const b = { a: 1, c: 2 };
-// const { a, ...rest } = b;
+const b = { a: 1, c: 2 };
+const { c, ...rest } = b;
 
-// const arr = [1, 2, 3];
-// const arr2 = [...arr, 4];
+const arr = [, 2, 3];
+const arr2 = [...arr, 4];
 
-// const hello = { a: "1", b: 2, c: 3 }; // -> ['a', 'b', 'c']
-// //hello["a"] === hello.a === "1";
+const hello = { a: "1", b: 2, c: 3 }; // -> ['a', 'b', 'c']
+//hello["a"] === hello.a === "1";
 
-// const keyeerGuih = Object.keys(hello); //-> ['a', 'b', 'c']
-// console.log(keyeerGuih);
+const keyeerGuih = Object.keys(hello); //-> ['a', 'b', 'c']
+console.log(keyeerGuih);
 
-// const valueguih = Object.values(hello); // -> ["1", 2, 3]
-// console.log(hello);
+const valueguih = Object.values(hello); // -> ["1", 2, 3]
+console.log(hello);
 
-// const entriesGuih = Object.entries(hello);
-// console.log(hello);
-
-// // keyeerGuih.map((el) => {
-
-// // hello[el] = 4;
-
-// // });
+const entriesGuih = Object.entries(hello);
+console.log(hello);
 
 // keyeerGuih.map((el) => {
-//   console.log(hello[el]);
+
+// hello[el] = 4;
+
 // });
+
+keyeerGuih.map((el) => {
+  console.log(hello[el]);
+});
